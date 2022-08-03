2199bbf2c6a00d306e7bbc57c44ad03c60c7af30
luis913 <luis@replikasoftware.com>
2022-08-02 19:56:02 -0700

    adding condition when there is no previous file

--- notes.sh
+++ notes.sh
@@ -3,6 +3,13 @@
 TODAY="$(date "+%d-%m-%Y")"
 FILE_NAME="notes$TODAY.md"
 
+
+# what do to if there is no file to begin with
+if [ $# -eq 0 ]; then
+   echo "No arguments provided, creating new notes file! (⌐■_■)っ🍺"
+   touch $FILE_NAME
+fi
+
 # phase 2 will involve getting the most recent file, maybe?
 cp $1 $FILE_NAME
 
