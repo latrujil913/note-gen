c69256e788cd1b7a4a87c4cd5d43c4eb762f91c2
luis913 <luis@replikasoftware.com>
2022-08-24 20:07:04 -0700

    added base case, to create a file with notes

--- notes.sh
+++ notes.sh
@@ -4,13 +4,32 @@ TODAY="$(date "+%d-%m-%Y")"
 FILE_NAME="notes$TODAY.md"
 
 
-# what do to if there is no file to begin with
-if [ $# -eq 0 ]; then
-   echo "No arguments provided, creating new notes file! (⌐■_■)っ🍺"
-   touch $FILE_NAME
-fi
-
 # phase 2 will involve getting the most recent file, maybe?
-cp $1 $FILE_NAME
+#if [ $# -eq 1 ]; then
+#   cp $1 $FILE_NAME
+#fi
 
+while [ ! $# -eq 0 ]
+do
+	case "$1" in
+		--android | -a)
+         echo 'hello a'
+			exit
+			;;
+		--ios | -i)		
+         echo 'hello i'
+			exit
+			;;
+      --help | -h)
+			echo "./notes.sh [-a|--android]"
+			exit
+			;;
+    *)
+         echo "No arguments provided, creating new notes file! (⌐■_■)っ🍺"
+         touch $FILE_NAME
+			exit
+			;;
+	esac
+	shift
+done
 # TODO: only copy over specific section in this file
